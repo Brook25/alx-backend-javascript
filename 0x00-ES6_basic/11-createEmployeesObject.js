a0427f2998148b3fff8e5a234b05987b468feba4
Brook25 <bekelebrook24@gmail.com>
2023-03-05 15:12:56 +0000

    return an object

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,5 @@
-export default function createEmployeesObject (departmentName, employees) {
-  return {
+export default function createEmployeesObject(departmentName, employees) {
+    return {
   [`${departmentName}`]: employees,
   };
 }
