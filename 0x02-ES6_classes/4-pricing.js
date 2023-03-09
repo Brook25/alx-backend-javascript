df9a0bd6894cf49b1836bf9bc9432dd87b9bb7c9
Brook25 <bekelebrook24@gmail.com>
2023-03-09 07:04:16 +0000

    pricing class

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,5 +1,3 @@
-import Currency from './3-currency.js';
-
 export default class Pricing {
   constructor(amount, currency) {
     this._amount = amount;
@@ -7,25 +5,25 @@ export default class Pricing {
   }
   
   get amount() {
-  return this._amount;
+    return this._amount;
   }
 
   set amount(amt) {
-  this._amount = amt;
+    this._amount = amt;
   }
-  
+
   get currency() {
-  return this._currency;
+    return this._currency;
   }
 
   set currency(curr) {
-  this._currency = curr;
+    this._currency = curr;
   }
 
   displayFullPrice() {
     return `${this._amount} ${this._currency._name} (${this._currency._code})`;
   }
-  
+ 
   static convertPrice (amount, conversionRate) {
     return amount * conversionRate;
   }
