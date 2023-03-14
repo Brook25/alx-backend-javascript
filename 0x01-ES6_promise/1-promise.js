1715269e548557562286646d941565553a9f25fa
Brook25 <bekelebrook24@gmail.com>
2023-03-14 20:29:37 +0000

    funciton that takes an arguent and returns a promise object

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,14 @@
-export default function promise1(success) {
-  return new Promise(function(resolve, reject) {
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
     if (success == true) {
+      
       resolve({ status: 200,
         body: 'Success',
-      });
+      
+     });
     }
-   else {
-     reject(new Error('The fake API is not working currently'));
-   }
+    else {
+      reject(new Error('The fake API is not working currently'));
+    }
   });
 }
