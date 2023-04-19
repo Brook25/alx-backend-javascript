5e2ff2551bcef0b0084d478faa4285ae81f9ab0c
Brook25 <bekelebrook24@gmail.com>
2023-04-19 19:19:39 +0000

    All tasks done

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -8,6 +8,6 @@ export default function handleResponseFromAPI(promise) {
      (reject) => {
       return new Error()}
      )
-   console.log("Got a response from the API");
+   console.log('Got a response from the API');
    return prom;
 }
