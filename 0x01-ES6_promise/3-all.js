be2fd0aeb26229f9ff74703ae2a99d499d1cd2d5
Brook25 <bekelebrook24@gmail.com>
2023-04-25 12:04:18 +0000

    option

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,3 +1,6 @@
 import { uploadPhoto, createUser } from 'utils.js';
 
-Promise.all([uploadPhoto(), createUser()]).then((lst) => { console.log(`${lst[0].body} ${lst[1].firstName} ${lst[1].lastName}`); }).catch(() => console.log('Signup system offline'));
+Promise.all([uploadPhoto(), createUser()]).then((lst) => {
+  console.log(`${lst[0].body} ${lst[1].firstName} ${lst[1].lastName}`);
+})
+  .catch(() => console.log('Signup system offline'));
