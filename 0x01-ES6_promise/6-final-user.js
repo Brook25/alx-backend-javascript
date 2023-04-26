96c7c02b0886fd94f6d98b6363b9e678919a3ad6
Brook25 <bekelebrook24@gmail.com>
2023-04-26 09:34:11 +0000

    edit

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,17 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
+
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((results) => {
       const ret = results.map((val) => {
-        const item = { ...val };
-        if ('reason' in item) {
-          item.value = String(val.reason); delete item.reason;
-        } return item;
+        if ('reason' in val) {
+          const item = { ...val };
+          item.value = String(item.reason); delete item.reason;
+          return item;
+        } return val;
       }); return ret;
     });
 }
+
