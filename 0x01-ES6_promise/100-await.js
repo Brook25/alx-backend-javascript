8c7048483e81c4e01a2d7ae5d09ec53c9eaa788c
Brook25 <bekelebrook24@gmail.com>
2023-04-26 13:39:16 +0000

    edit

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,20 +1,9 @@
-//import * as module from './utils';
-
 import { uploadPhoto, createUser } from './utils';
-/*export default async function asyncUploadUser() {
-  const uPhoto = module.uploadPhoto;
-  const cUser = module.createUser;
-
-  const res = { photo: null, user: null };
-  const x = await uPhoto().catch(() => null);
-  if (x !== null) {
-    const y = await cUser().catch(() => null);
-    if (y) { res.photo = x; res.user = y; return res; }
-  }
-  return res;
-}*/
 
 export default async function asyncUploadUser() {
-        return await Promise.all([uploadPhoto(), createUser()]).then((res) => ({ photo: res[0], user: res[1] })).catch(() => ({ photo: null, user: null }));
+  const response = await Promise.all([uploadPhoto(), createUser()])
+    .then((res) => ({ photo: res[0], user: res[1] }))
+    .catch(() => ({ photo: null, user: null }));
+  return response;
 }
 
