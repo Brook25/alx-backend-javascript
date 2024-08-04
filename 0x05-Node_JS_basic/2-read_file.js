544fd4241717f98f3e0a679a420c40ceb2b1387a
Brook25 <bekelebrook24@gmail.com>
2024-08-04 15:02:27 +0000

    elegant

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,28 +1,31 @@
 fs = require('fs');
 
-fs.readFile('database.csv', 'utf-8', (err, data) => { 
-  if (err)
-    throw new Error('Cannot load the database');
-  const lines = data.split('\n').slice(1);
-  console.log('Number of students: ' + lines.length);
-  student_data = lines.map((elem) => {
-    return elem.split(',')
-  });
-  fields = student_data.map((elem) => {
-    return elem[3];
-  });
-  uniques = Array.from(new Set(fields));
-  uniques.sort();
-  uniques.forEach((elem) => {
-    student_count = student_data.reduce((count, value) => {
-      return value[3] === elem ? count + 1 : count;
-    }, 0
-    );
-  studentsInField = []
-  student_data.forEach((student) => {
-    if (student[3] === elem)
-	  studentsInField.push(student[0]);
+function countStudents(path) {
+  fs.readFile(path, 'utf-8', (err, data) => { 
+    if (err)
+      throw new Error('Cannot load the database');
+    const lines = data.split('\n').slice(1, -1);
+    console.log('Number of students: ' + lines.length);
+    student_data = lines.map((elem) => {
+      return elem.split(',')
     });
-  console.log(`Number of students in ${elem}: ${student_count}. List: ${studentsInField.join(', ')}`);
+    fields = student_data.map((elem) => {
+    return elem[3];
+    });
+    uniques = Array.from(new Set(fields));
+    uniques.sort();
+    uniques.forEach((elem) => {
+      student_count = student_data.reduce((count, value) => {
+        return value[3] === elem ? count + 1 : count;
+      }, 0
+      );
+    studentsInField = []
+    student_data.forEach((student) => {
+      if (student[3] === elem)
+	studentsInField.push(student[0]);
+      });
+    console.log(`Number of students in ${elem}: ${student_count}. List: ${studentsInField.join(', ')}`);
 });
 });
+}
+exports.countStudents = countStudents;
